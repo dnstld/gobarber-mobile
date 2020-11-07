d3fe6164b0bf87e7d1b76414f6f10043921fb917
Denis Toledo <contato@denistoledo.com.br>
2020-11-07 15:20:25 -0300

    inti project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {View} from 'react-native';
+
+const App: () => React.FC = () => <View />;
+
+export default App;
