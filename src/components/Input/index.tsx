1efcf5feb915032cc66805e5b076f5e9872d1cf6
Denis Toledo <contato@denistoledo.com.br>
2020-11-09 18:20:02 -0300

    init input and button components

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Input: React.FC = () => {
+  return (
+    <Container>
+    </Container>
+  );
+}
+
+export default Input;
