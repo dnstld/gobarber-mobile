9650cdbbd8dec1940f26b3e31c3a9231c10ea48a
Denis Toledo <contato@denistoledo.com.br>
2020-11-09 19:20:58 -0300

    input component more progress

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -13,8 +13,8 @@ const SignIn: React.FC = () => {
     <Container>
       <Image source={logoImg} />
       <Title>Faça seu logon</Title>
-      <Input />
-      <Input />
+      <Input name="email" icon="mail" placeholder="Email" />
+      <Input name="password" icon="lock" placeholder="Senha" />
 
       <Button onPress={() => {}}>Entrar</Button>
     </Container>
