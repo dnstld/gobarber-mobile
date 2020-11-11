0a4a232c080d863274084c73d0905ae467b5a92d
Denis Toledo <contato@denistoledo.com.br>
2020-11-11 18:32:29 -0300

    add signup UI

--- src/pages/SignUp/styles.ts
+++ src/pages/SignUp/styles.ts
@@ -1,3 +1,36 @@
 import styled from 'styled-components/native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
-export const Container = styled.View``;
+export const Container = styled.View`
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  padding: 0 30px;
+`;
+
+export const Title = styled.Text`
+  font-size: 24px;
+  color: #f4ede8;
+  font-family: 'Roboto-Medium';
+  margin: 64px 0 24px;
+`;
+
+export const BackToSignIn = styled.TouchableOpacity`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: #312e38;
+  border-top-width: 1px;
+  padding: 16px 0 ${8 + getBottomSpace()}px;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+`;
+
+export const BackToSignInText = styled.Text`
+  color: #fff;
+  font-size: 18px;
+  font-family: 'Roboto-Regular';
+  margin-left: 16px;
+`;
