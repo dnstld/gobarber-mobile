dbc4c83b59d1c2923ff78fc05dc6451ff02140dc
Denis Toledo <contato@denistoledo.com.br>
2020-11-11 18:23:57 -0300

    add signin UI

--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components/native';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
 export const Container = styled.View`
   flex: 1;
@@ -12,4 +13,34 @@ export const Title = styled.Text`
   color: #f4ede8;
   font-family: 'Roboto-Medium';
   margin: 64px 0 24px;
+`;
+
+export const ForgotPassword = styled.TouchableOpacity`
+  margin-top: 24px;
+`;
+
+export const ForgotPasswordText = styled.Text`
+  color: #f4ede8;
+  font-size: 16px;
+  font-family: 'Roboto-Medium';
+`;
+
+export const CreateAccountButton = styled.TouchableOpacity`
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: #312e38;
+  border-top-width: 1px;
+  padding: 16px 0 ${8 + getBottomSpace()}px;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+`;
+
+export const CreateAccountButtonText = styled.Text`
+  color: #ff9000;
+  font-size: 18px;
+  font-family: 'Roboto-Regular';
+  margin-left: 16px;
 `;
